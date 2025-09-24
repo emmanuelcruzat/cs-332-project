6990d8396acde06052393e428c384834c3806e09
Emmanuel Cruzat <etc@njit.edu>
2025-09-23 21:39:39 -0400

    Basic data reading

--- fcfs.py
+++ fcfs.py
@@ -4,6 +4,12 @@ import time
 import sys
 from datetime import datetime
 
+class Process:
+    def __init__(self, pid, burst_time):
+        self.pid = pid
+        self.burst_time = burst_time
+        
+
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 12345
 
@@ -13,13 +19,27 @@ process_queue = []
 queue_lock = threading.Lock()
 
 
-def scheduler():
+def scheduler(client_socket):
+    while True:
+        data = client_socket.recv(1024).decode()
+
+        if data == "END":
+            break;
+        
+        #convert data in process objects
+        #put process objects in the queue
+        #simulates running the processes??
+
+        print(f"Collected data={data}")
+
 # ...
 
-def shell():
+#def shell():
 # ...
 
 def main():
+    global client_socket
+
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
@@ -30,17 +50,17 @@ def main():
 
     print("Connected to the server")
 
-    scheduler_thread = threading.Thread(target=scheduler)
-    shell_thread = threading.Thread(target=shell)
+    scheduler_thread = threading.Thread(target=scheduler(client_socket))
+    #shell_thread = threading.Thread(target=shell)
     scheduler_thread.start()
-    shell_thread.start()
+    #shell_thread.start()
 
     # ...
 
-    shell_thread.join()
+    #shell_thread.join()
     scheduler_thread.join()
     client_socket.close()
-    log_file.close()
+    # log_file.close()
 
 if __name__ == "__main__":
     main()
