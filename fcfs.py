ddbf703e18a1ecce947d0303f555441479491de9
Emmanuel Cruzat <etc@njit.edu>
2025-09-26 16:22:53 -0400

    working fcfs

--- fcfs.py
+++ fcfs.py
@@ -8,6 +8,9 @@ class Process:
     def __init__(self, pid, burst_time):
         self.pid = pid
         self.burst_time = burst_time
+
+    def print_details(self):
+        print(f"({self.pid}, {self.burst_time}) ")
         
 
 SERVER_IP = "127.0.0.1"
@@ -20,17 +23,64 @@ queue_lock = threading.Lock()
 
 
 def scheduler(client_socket):
+
+    isEnd = False;
+    isPid = True;
+
     while True:
         data = client_socket.recv(1024).decode()
 
-        if data == "END":
-            break;
-        
-        #convert data in process objects
-        #put process objects in the queue
-        #simulates running the processes??
+        pid_string = ""
+        burst_string=""
 
-        print(f"Collected data={data}")
+        pid_list = []
+        burst_list = []
+
+        for char in data:
+            #print(f"char {char} pid_string {pid_string} burst_string {burst_string}")
+            if char == "E":
+                isEnd = True;
+                break;
+            elif isPid:
+                if char != " ":
+                    pid_string += char
+                else:
+                    isPid = False
+                    pid_list.append(int(pid_string))
+                    pid_string = ""
+            else:
+                if char != " ":
+                    burst_string += char
+                else:
+                    isPid = True
+                    burst_list.append(int(burst_string))
+                    burst_string = ""
+
+        #pid, burst_time = data.rstrip().split(None, 1)
+        #print(f"pid_list={pid_list} burst_list={burst_list}")
+        # make new process objects and append to queue
+        for i in range(len(pid_list)):
+            process_queue.append(Process(int(pid_list[i]), int(burst_list[i])))
+
+        for i in range(len(process_queue)):
+            process_queue[i].print_details()
+
+        if len(process_queue) > 0:
+            front = process_queue.pop(0)
+        elif isEnd == True:
+            print("IT IS THE END.")
+            break;
+        else:
+            print("Didn't detect END")
+            break;
+
+        print(f"PID = {front.pid}")
+        print(f"Burst time = {front.burst_time}")
+        print(f"Sleeping for = {front.burst_time}s")
+
+        time.sleep(front.burst_time)
+
+        print(f"Awoke.")
 
 # ...
 
